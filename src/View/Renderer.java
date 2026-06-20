b8447785abd656330c6195e2bbb5ff85541c513c
denegas <den2reserv@gmail.com>
2026-06-20 16:53:05 +0500

    fear(UI): added Renderer prototype

---
+++ src/View/Renderer.java
@@ -0,0 +1,27 @@
+package View;
+
+import Controller.Simulation;
+import Model.Coordinates;
+import Model.Entity;
+import Model.EntityType;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public final class Renderer {
+    private final static int mapSize = Simulation.getMapSize();
+    private Renderer(){}
+ public static void render(Map<Coordinates, Entity> map){
+     Map<Coordinates,Entity> sortedMap = new TreeMap<>(map);
+     for (int i = 0; i< mapSize;i++){
+         for (int j = 0;j<mapSize;j++){
+             if(sortedMap.get(new Coordinates(i,j)) == null) System.out.print(EntityType.VOID.getEntityView() + " ");
+             else {
+                 System.out.print(sortedMap.get(new Coordinates(i, j)).getType().getEntityView() + " ");
+             }
+         }
+         System.out.println();
+     }
+
+ }
+}
