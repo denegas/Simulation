f64c8d2e8574816600d5e6ec565f25b4b0e19a43
denegas <den2resev@gmail.com>
2026-07-09 17:07:00 +0500

    feat(core): added first working turn food creator

--- src/Controller/TurnEntityCreator.java
+++ src/Controller/TurnEntityCreator.java
@@ -11,10 +11,14 @@ public class TurnEntityCreator extends EntityCreator implements Action {
     public TurnEntityCreator(){
         super();
     }
+    private final int MAX_TURNS_WITHOUT_FOOD = 3;
+    private int withoutHerbivoreFoodCounter = 0;
+    private int withoutPredatorFoodCounter = 0;
 
 
     @Override
     public void execute(EntityMap map) {
+
 
         boolean hasPredatorFood = false;
         boolean hasHerbivoreFood = false;
@@ -31,8 +35,17 @@ public class TurnEntityCreator extends EntityCreator implements Action {
       }
         Map<EntityType,Boolean> hasFoodForCreature = Map.of(EntityType.HERBIVORE,hasHerbivoreFood,EntityType.PREDATOR,hasPredatorFood);
         for(var entry:hasFoodForCreature.entrySet()){
+
+            if (withoutPredatorFoodCounter > MAX_TURNS_WITHOUT_FOOD){
+                addMissingFood(EntityType.PREDATOR,map);
+                withoutPredatorFoodCounter = 0;
+            }
+            if ( withoutHerbivoreFoodCounter > MAX_TURNS_WITHOUT_FOOD){
+                addMissingFood(EntityType.HERBIVORE,map);
+                withoutHerbivoreFoodCounter  = 0;
+            }
             if(!entry.getValue()){
-               addMissingFood(entry.getKey(),map);
+                 increaseCounter(entry.getKey());
             }
         }
       Simulation.setMap(map);
@@ -48,11 +61,23 @@ public class TurnEntityCreator extends EntityCreator implements Action {
         }
     }
     private EntityType getFoodType(EntityType creatureType){
-        return switch (creatureType){
+        return switch (creatureType) {
             case HERBIVORE -> EntityType.GRASS;
             case PREDATOR -> EntityType.HERBIVORE;
             default -> throw new IllegalArgumentException("unexpected creature type: " + creatureType);
         };
+    }
+    private void increaseCounter(EntityType entityType) {
+ switch (entityType){
+     case HERBIVORE: withoutHerbivoreFoodCounter++;
+     break;
+     case PREDATOR:
+         withoutPredatorFoodCounter++;
+         break;
+     default:
+         throw new IllegalArgumentException("unexpected entityType: " + entityType);
+
+ }
     }
 }
 
